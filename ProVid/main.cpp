d1e8a9a981b0caba402722a507528fb3c127d753
Xard <michal.wendel@gmail.com>
2023-01-25 23:36:36 +0100

    add opencv and build it; unify installation directory for 3rd party libs

--- ProVid/main.cpp
+++ ProVid/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 #include <glfw/glfw3.h>
+#include <opencv2/videoio.hpp>
 
 int main() {
     const auto result = glfwInit();
     std::cout << "GLFW init: " << (result == GLFW_TRUE) << std::endl;
+
     return 0;
 }
