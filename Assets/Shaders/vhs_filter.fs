1a57060d99fae41a937c70146ab2708aea13b68b
Xard <michal.wendel@gmail.com>
2023-01-27 18:23:29 +0100

    split image channels

--- Assets/Shaders/vhs_filter.fs
+++ Assets/Shaders/vhs_filter.fs
@@ -6,7 +6,7 @@ in vec2 textureCoords;
 
 layout(location = 0) out vec3 outColor;
 
-vec4 makeBlur(vec2 baseTextureCoords, vec2 blurStep)
+vec4 makeBlur(vec2 baseTextureCoords, vec2 imageTexelSize)
 {
     const float blurKernel[25] = float[](
         1, 4, 7, 4, 1,
@@ -22,7 +22,7 @@ vec4 makeBlur(vec2 baseTextureCoords, vec2 blurStep)
         for (int j = -2; j < 3; ++j)
         {
             float blurWeight = blurKernel[(i + 2) + (j + 2) * 5] / 273.0;
-            color += texture(image, baseTextureCoords + blurStep * vec2(i, j)) * blurWeight;
+            color += texture(image, baseTextureCoords + imageTexelSize * vec2(i, j)) * blurWeight;
         }
     }
 
@@ -31,10 +31,12 @@ vec4 makeBlur(vec2 baseTextureCoords, vec2 blurStep)
 
 void main()
 {
-    ivec2 imageSize = textureSize(image, 0);
-    vec2 blurStep = 1.0 / imageSize;
+    vec2 imageTexelSize = 1.0 / textureSize(image, 0);
+    vec2 channelOffset = vec2(0.0, imageTexelSize.y * 4.0f);
 
-    vec4 color = makeBlur(textureCoords, blurStep);
+    vec4 red = makeBlur(textureCoords - channelOffset, imageTexelSize);
+    vec4 green = makeBlur(textureCoords, imageTexelSize);
+    vec4 blue = makeBlur(textureCoords + channelOffset, imageTexelSize);
 
-    outColor = color.xyz;
+    outColor = vec3(red.x, green.y, blue.z);
 }
